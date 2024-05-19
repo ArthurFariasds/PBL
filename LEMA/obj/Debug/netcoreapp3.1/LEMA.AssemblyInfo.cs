ffa44d4d145765091b073d7225b2c806a33169b5
Maycon Silva Cardoso <maycon_silva2003@hotmail.com>
2024-05-18 21:37:45 -0300

    String conexão atualizada

--- LEMA/obj/Debug/netcoreapp3.1/LEMA.AssemblyInfo.cs
+++ LEMA/obj/Debug/netcoreapp3.1/LEMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LEMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91607c0d79e6bf1056aad97d5b9b7fefa84844bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LEMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LEMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
