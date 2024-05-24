28634fb61075e0ba422fa79ed27e96289b5b54b4
Arthur Farias <arthur.farias01@gmail.com>
2024-05-24 12:33:09 -0300

    Tela Sobre

---
+++ LEMA/Controllers/SobreController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PBL.DAO;
+using PBL.Models;
+
+namespace PBL.Controllers
+{
+    public class SobreController : Controller
+    {
+        public IActionResult Index()
+        {
+            UsuarioDAO usuarioDao = new UsuarioDAO();
+
+            UsuarioViewModel usuario = usuarioDao.Consulta(HelperControllers.GetUsuarioId(HttpContext.Session));
+            ViewBag.Perfil = usuario.Perfil;
+            ViewBag.Imagem64 = usuario.ImagemEmBase64;
+            ViewBag.Logado = HelperControllers.VerificaUserLogado(HttpContext.Session);
+            ViewBag.Username = HelperControllers.GetUsername(HttpContext.Session);
+            ViewBag.IdUsuario = HelperControllers.GetUsuarioId(HttpContext.Session);
+
+            return View();
+        }
+    }
+}
